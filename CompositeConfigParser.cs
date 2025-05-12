fe17820377e85daec4615759656417f8e96164b6
d.bogdanov <d.bodgdanov@nexters.com>
2025-05-12 12:01:40 +0400

    add composite config parser

---
+++ CompositeConfigParser.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Azzazelloqq.Config
+{
+public class CompositeConfigParser : IConfigParser
+{
+	private readonly IConfigParser[] _parsers;
+
+	public CompositeConfigParser(params IConfigParser[] parsers)
+	{
+		_parsers = parsers;
+	}
+
+	public IConfigPage[] Parse()
+	{
+		return _parsers
+			.SelectMany(p => p.Parse())
+			.ToArray();
+	}
+
+	public async Task<IConfigPage[]> ParseAsync(CancellationToken token)
+	{
+		var tasks = _parsers
+			.Select(p => p.ParseAsync(token))
+			.ToArray();
+
+		var results = await Task.WhenAll(tasks);
+		return results
+			.SelectMany(r => r)
+			.ToArray();
+	}
+}
+}
